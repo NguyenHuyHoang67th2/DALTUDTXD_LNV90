ad287222be47d551def60cd0f27043ec8337739d
NguyenHuyHoang67th2 <0024167@st.huce.edu.vn>
2026-06-26 00:21:44 +0700

    homepageview

--- Views/Pages/NoiLucPage.xaml.cs
+++ Views/Pages/NoiLucPage.xaml.cs
@@ -1,27 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using DALTUDTXD_KIEMTRADAMTHEP_0024167_67TH2.ViewModels;
 
 namespace DALTUDTXD_KIEMTRADAMTHEP_0024167_67TH2.Views.Pages
 {
-    /// <summary>
-    /// Interaction logic for NoiLucPage.xaml
-    /// </summary>
-    public partial class NoiLucPage : Window
+    public partial class NoiLucPage : Page
     {
         public NoiLucPage()
         {
             InitializeComponent();
+            this.DataContext = BeamViewModel.Instance;
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e) { }
+
+        private void rdoManual_Checked(object sender, RoutedEventArgs e)
+        {
+            if (PanelManualInput != null)
+            {
+                PanelManualInput.Visibility = Visibility.Visible;
+                PanelEtabsInput.Visibility = Visibility.Collapsed;
+                btnSave.IsEnabled = true;
+            }
+        }
+
+        private void rdoEtabs_Checked(object sender, RoutedEventArgs e)
+        {
+            if (PanelManualInput != null)
+            {
+                PanelManualInput.Visibility = Visibility.Collapsed;
+                PanelEtabsInput.Visibility = Visibility.Visible;
+                btnSave.IsEnabled = false;
+            }
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Lưu thông số nội lực thành công!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void btnQuayLai_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.NavigationService != null)
+                this.NavigationService.Navigate(new HomePage());
         }
     }
 }
