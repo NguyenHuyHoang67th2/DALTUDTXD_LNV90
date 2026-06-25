00672bb8d88720c4c8c10b5fc21cc3f65bcf68ab
NguyenHuyHoang67th2 <0024167@st.huce.edu.vn>
2026-06-25 21:03:38 +0700

    Add project files.

---
+++ Views/Pages/KichThuocPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DALTUDTXD_KIEMTRADAMTHEP_0024167_67TH2.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for KichThuocPage.xaml
+    /// </summary>
+    public partial class KichThuocPage : Window
+    {
+        public KichThuocPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
