ac855f00675eb78ddb67cafd1f0169eea7d7f64b
AD <AD@ADMIN-PC>
2026-06-25 21:38:54 +0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DALTUDTXD_KIEMTRADAMTHEP_0024167_67TH2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Models/BeamResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALTUDTXD_KIEMTRADAMTHEP_0024167_67TH2.Models
+{
+    internal class BeamResult
+    {
+    }
+}
