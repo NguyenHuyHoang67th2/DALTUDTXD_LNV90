ad1cf1e71af8943a9e7341fb6acad507d365cabf
NguyenQuangMinh-67TH2-0036967 <0036967@st.huce.edu.vn>
2026-06-26 00:15:33 +0700

    viewmodelbase

--- ViewModels/ViewModelBase.cs
+++ ViewModels/ViewModelBase.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 
 namespace DALTUDTXD_KIEMTRADAMTHEP_0024167_67TH2.ViewModels
 {
-    internal class ViewModelBase
+    public class ViewModelBase : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
